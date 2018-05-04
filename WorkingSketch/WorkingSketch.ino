4a752951f03a70083d6b0f1137d3b4589600ea5a
airibot <airigare@mikmak.cc>
2018-05-05 01:41:08 +0200

    Enable Pumping
    Add Pumping Function

--- WorkingSketch/WorkingSketch.ino
+++ WorkingSketch/WorkingSketch.ino
@@ -15,7 +15,7 @@
  /*
 This example does a test of the TCP client capability:
   * Initialization
-  * Optional: SSID scan
+  * Optional: SSID scan 
   * AP connection
   * DHCP printout
   * DNS lookup
@@ -49,6 +49,8 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
 #define WLAN_PASS       "Nkensleg8"
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
+
+#define RELAIS_PIN 3
 
 #define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
                                    // received before closing the connection.  If you know the server
@@ -97,19 +99,78 @@ void hang(const __FlashStringHelper *str) {
   for(;;);
 }
 
-void setup(void)
-{
-  Serial.begin(115200);
-  Serial.println(F("Hello, CC3000!\n")); 
 
-  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+
+// Return true if enabled and connected, false otherwise.
+boolean enableWiFi() {
+  Serial.print(("Initializing CC3000..."));
+  if(!cc3000.begin()) { hang(F("failed.")); } else {Serial.print(F("OK\r\n"));};
+
+  Serial.print(("Deleting old connection profiles..."));
+  if(!cc3000.deleteProfiles()) { hang(F("failed.")); } else {Serial.print(F("OK\r\n"));};
+
+  Serial.print(("Connecting to network..."));
+  /* NOTE: Secure connections are not available in 'Tiny' mode! */
+  if(!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) { hang(F("failed.")); } else {Serial.print(F("OK\r\n"));};
+
+  Serial.print(("Requesting address from DHCP server..."));
   
+  for(t=millis(); !cc3000.checkDHCP() && ((millis() - t) < dhcpTimeout); delay(100));
+  if(!cc3000.checkDHCP()) { hang(F("failed.")); } else { Serial.print(F("OK\r\n")); };
+
+
+  /* Display the IP address DNS, Gateway, etc. */  
+  while (! displayConnectionDetails()) {
+    delay(1000);
+  }
+  
+  // Return success, the CC3000 is enabled and connected to the network.
+  return true;
 }
 
-// Take a sensor reading and send it to the server.
-bool logSensorReading() {
+// Disconnect from wireless network and shut down the CC3000.
+void shutdownWiFi() {
+  // Disconnect from the AP if connected.
+  // This might not be strictly necessary, but I found
+  // it was sometimes difficult to quickly reconnect to
+  // my AP if I shut down the CC3000 without first
+  // disconnecting from the network.
 
-  ip = 0;
+  // Wait for the CC3000 to finish disconnecting before
+  // continuing.
+
+    Serial.println(("Disconnecting...")); 
+    cc3000.disconnect();
+  
+  // Shut down the CC3000.
+  wlan_stop();
+  
+  Serial.println(F("CC3000 shut down.")); 
+  delay(100);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Helper Functions
+*/
+/**************************************************************************/
+bool displayConnectionDetails(void) {
+  uint32_t addr, netmask, gateway, dhcpserv, dnsserv;
+
+  if(!cc3000.getIPAddress(&addr, &netmask, &gateway, &dhcpserv, &dnsserv))
+    return false;
+
+  Serial.print(F("IP Addr: ")); cc3000.printIPdotsRev(addr);
+  Serial.print(F("\r\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+  Serial.print(F("\r\nGateway: ")); cc3000.printIPdotsRev(gateway);
+  Serial.print(F("\r\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+  Serial.print(F("\r\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+  Serial.println();
+  return true;
+}
+
+uint32_t getIP (void) {
+    ip = 0;
   // Try looking up the website's IP address
   Serial.print(WEBSITE); Serial.print(F(" -> "));
   while (ip == 0) {
@@ -118,10 +179,22 @@ bool logSensorReading() {
     }
     delay(500);
   }
+  return ip;
+}
 
-  // Temporarly overwrite ip
-  //ip = 192,168,1,112;
-  // Connect to the server and send the reading.
+
+/**************************************************************************/
+/*!
+    @ WWW Functions
+*/
+/**************************************************************************/
+/**************************************************************************/
+/*!
+    @ Call Mom
+*/
+bool callMama() {
+
+  uint32_t ip = getIP();
 
   Adafruit_CC3000_Client www = cc3000.connectTCP(ip, PORT);
   if (www.connected()) {
@@ -136,13 +209,7 @@ bool logSensorReading() {
     Serial.println(F("Connection failed"));
   }
 
-  Serial.println(F("-------------------------------------"));
-  
 
-//  while (www.available()) {
-//      char c = www.read();
-//      Serial.print(c);
-//  }
 
   // Note that if you're sending a lot of data you
   // might need to tweak the delay here so the CC3000 has
@@ -194,63 +261,119 @@ unsigned long startTime = millis();
   return true;
 }
 
+/**************************************************************************/
+/*!
+    @ Get Watering
+*/
+long getPumpTime() {
 
-// Return true if enabled and connected, false otherwise.
-boolean enableWiFi() {
-  Serial.print(("Initializing CC3000..."));
-  if(!cc3000.begin()) { hang(F("failed.")); } else {Serial.print(F("OK\r\n"));};
+  long r;
 
-  Serial.print(("Deleting old connection profiles..."));
-  if(!cc3000.deleteProfiles()) { hang(F("failed.")); } else {Serial.print(F("OK\r\n"));};
+uint32_t ip = getIP();
 
-  Serial.print(("Connecting to network..."));
-  /* NOTE: Secure connections are not available in 'Tiny' mode! */
-  if(!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) { hang(F("failed.")); } else {Serial.print(F("OK\r\n"));};
-
-  Serial.print(("Requesting address from DHCP server..."));
-  
-  for(t=millis(); !cc3000.checkDHCP() && ((millis() - t) < dhcpTimeout); delay(100));
-  if(!cc3000.checkDHCP()) { hang(F("failed.")); } else { Serial.print(F("OK\r\n")); };
-
-
-  /* Display the IP address DNS, Gateway, etc. */  
-  while (! displayConnectionDetails()) {
-    delay(1000);
+  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, PORT);
+  if (www.connected()) {
+    www.fastrprint(F("GET "));
+    www.fastrprint("/mikmakAPI/airigare/Station/getPumpTime");
+    www.fastrprint(F(" HTTP/1.1\r\n"));
+    www.fastrprint(F("Host: ")); www.fastrprint(WEBSITE); www.fastrprint(F("\r\n"));
+    www.fastrprint(F("\r\n"));
+    www.println();
   }
+  else {
+    Serial.println(F("Connection failed"));
+  }
+
+
+  // Note that if you're sending a lot of data you
+  // might need to tweak the delay here so the CC3000 has
+  // time to finish sending all the data before shutdown.
+Serial.print(F("OK\r\nAwaiting response..."));
+unsigned long startTime = millis();
+  while((!www.available()) &&
+            ((millis() - startTime) < responseTimeout));
+            {
   
-  // Return success, the CC3000 is enabled and connected to the network.
-  return true;
+  // Close the connection to the server.
+  // Check HTTP status
+  char status[32] = {0};
+  www.readBytesUntil('\r', status, sizeof(status));
+  if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+    Serial.print(F("Unexpected response: "));
+    Serial.println(status);
+    return;
+  }
+
+  // Skip HTTP headers
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!www.find(endOfHeaders)) {
+    Serial.println(F("Invalid response"));
+    return;
+  }
+
+  // Allocate JsonBuffer
+  // Use arduinojson.org/assistant to compute the capacity.
+  const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+  DynamicJsonBuffer jsonBuffer(capacity);
+
+  // Parse JSON object
+  JsonObject& root = jsonBuffer.parseObject(www);
+  if (!root.success()) {
+    Serial.println(F("Parsing failed!"));
+    return;
+  }
+
+  // Extract values
+  Serial.println(F("Response:"));
+  Serial.println(root["pumpTime"].as<char*>());
+  delay(100);
+  r = root["pumpTime"];
+  //Serial.println(root["data"][0].as<char*>());
+  //Serial.println(root["data"][1].as<char*>());
+            }
+  
+  www.close();
+  return r;
 }
 
-// Disconnect from wireless network and shut down the CC3000.
-void shutdownWiFi() {
-  // Disconnect from the AP if connected.
-  // This might not be strictly necessary, but I found
-  // it was sometimes difficult to quickly reconnect to
-  // my AP if I shut down the CC3000 without first
-  // disconnecting from the network.
+void enablePump(int pumpTime) {
+  Serial.print("Start Pumping for: "); Serial.println(pumpTime, DEC);
+  digitalWrite(RELAIS_PIN, HIGH);
+  for(int x = 0; x < pumpTime; x = x + 1){
+delay(1000);
+}
+digitalWrite(RELAIS_PIN, LOW);
+Serial.print("Stop Pumping");
+}
 
-  // Wait for the CC3000 to finish disconnecting before
-  // continuing.
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println(F("Hello, CC3000!\n")); 
 
-    Serial.println(("Disconnecting...")); 
-    cc3000.disconnect();
-  
-  // Shut down the CC3000.
-  wlan_stop();
-  
-  Serial.println(F("CC3000 shut down.")); 
+  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+
+    pinMode(RELAIS_PIN, OUTPUT);
   delay(100);
+  digitalWrite(RELAIS_PIN, HIGH);
+  delay(1000);
+  digitalWrite(RELAIS_PIN, LOW);
+  
 }
 
 void loop(void)
 {
   if (sleepIterations == 0) {
     if (enableWiFi()) {
-      while(!logSensorReading()) {
-        Serial.println(F("Waiting for Server Response...")); 
-        delay(100);
-      }
+      
+      Serial.print(F("Calling Mama... "));
+      if(!callMama()) { hang(F("failed.")); } else {Serial.print(F("OK\r\n"));};
+      
+      Serial.print(F("Get Pump Time... "));
+      int pumpTime = int(getPumpTime());
+      if(pumpTime > 0) { Serial.print(F("OK\r\n")); } else {Serial.print(F("nOK\r\n"));};
+      if(pumpTime > 0) enablePump(pumpTime);
+      
     }
     shutdownWiFi();
   }
@@ -266,63 +389,4 @@ void loop(void)
 
   sleepIterations += 1;
 
-}
-
-/**************************************************************************/
-/*!
-    @brief  Begins an SSID scan and prints out all the visible networks
-*/
-/**************************************************************************/
-
-void listSSIDResults(void)
-{
-  uint32_t index;
-  uint8_t valid, rssi, sec;
-  char ssidname[33]; 
-
-  if (!cc3000.startSSIDscan(&index)) {
-    Serial.println(F("SSID scan failed!"));
-    delay(100);
-    return;
-  }
-
-  Serial.print(F("Networks found: ")); Serial.println(index);
-  Serial.println(F("================================================"));
-
-  while (index) {
-    index--;
-
-    valid = cc3000.getNextSSID(&rssi, &sec, ssidname);
-    
-    Serial.print(F("SSID Name    : ")); Serial.print(ssidname);
-    Serial.println();
-    Serial.print(F("RSSI         : "));
-    Serial.println(rssi);
-    Serial.print(F("Security Mode: "));
-    Serial.println(sec);
-    Serial.println();
-  }
-  Serial.println(F("================================================"));
-
-  cc3000.stopSSIDscan();
-}
-
-/**************************************************************************/
-/*!
-    @brief  Tries to read the IP address and other connection details
-*/
-/**************************************************************************/
-bool displayConnectionDetails(void) {
-  uint32_t addr, netmask, gateway, dhcpserv, dnsserv;
-
-  if(!cc3000.getIPAddress(&addr, &netmask, &gateway, &dhcpserv, &dnsserv))
-    return false;
-
-  Serial.print(F("IP Addr: ")); cc3000.printIPdotsRev(addr);
-  Serial.print(F("\r\nNetmask: ")); cc3000.printIPdotsRev(netmask);
-  Serial.print(F("\r\nGateway: ")); cc3000.printIPdotsRev(gateway);
-  Serial.print(F("\r\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
-  Serial.print(F("\r\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
-  Serial.println();
-  return true;
 }
